b95f9039cd41a1278c873c69f084b9f8a65ae859
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-12 20:09:08 +0200

    feat: Start implementing navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
-import { Button, Typography } from "@material-ui/core";
+import { Button, Typography, Box, Container } from "@material-ui/core";
 import styled from "styled-components";
 
-const TestButton = styled(Button)`
-  color: ${({ theme }) => theme.palette.primary.main};
+const NavButton = styled(Button)`
+  font-family: ${({ theme }) => theme.typography.h1.fontFamily};
 `;
 
 const Home = () => (
   <>
-    <Typography variant="h1">DANIELA BURKHARDT</Typography>
-    <Typography variant="h2">ÜbER MICH</Typography>
-    <TestButton>Hello, world!</TestButton>
+    <Box boxShadow={1}>
+      <Container maxWidth="lg">
+        <NavButton href="#ueber-mich">Über mich</NavButton>
+      </Container>
+    </Box>
+    <Container maxWidth="lg">
+      <Typography variant="h1">Daniela Burkhardt</Typography>
+      <Typography variant="h2">Über mich</Typography>
+    </Container>
   </>
 );
 
