54a88c2d32a1c8329220ca9196e1841e43296cf0
Felix Pojtinger <felix@pojtinger.com>
2020-09-12 22:01:47 +0200

    feat: Add sidebar menu on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
-import { Box, Button, Container, Toolbar, Typography } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Container,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  SwipeableDrawer,
+  Toolbar,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+import { Menu } from "@material-ui/icons";
 import Link from "next/link";
+import { useState } from "react";
 import styled from "styled-components";
 
 const Nav = styled(Box)`
@@ -19,32 +34,103 @@ const NavLogo = styled.img`
   cursor: pointer;
 `;
 
-const Home = () => (
-  <>
-    <Nav>
+const SliderListItemText = styled(ListItemText)`
+  > span {
+    font-family: ${({ theme }) => theme.typography.h1.fontFamily} !important;
+    text-transform: ${({ theme }) => theme.typography.h1.textTransform};
+    font-size: 1.05rem;
+  }
+`;
+
+const Home = () => {
+  const theme = useTheme();
+  const navMenuFullscreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const [navMenuOpen, setNavMenuOpen] = useState(false);
+
+  return (
+    <>
+      <Nav>
+        <Container maxWidth="lg">
+          <Toolbar disableGutters>
+            {navMenuFullscreen ? (
+              <>
+                <IconButton onClick={() => setNavMenuOpen(true)}>
+                  <Menu />
+                </IconButton>
+
+                <SwipeableDrawer
+                  open={navMenuOpen}
+                  onClose={() => setNavMenuOpen(false)}
+                  onOpen={() => setNavMenuOpen(true)}
+                >
+                  <List component="nav">
+                    <ListItem
+                      button
+                      component="a"
+                      href="#ueber-mich"
+                      onClick={() => setNavMenuOpen(false)}
+                    >
+                      <SliderListItemText>Über mich</SliderListItemText>
+                    </ListItem>
+                    <ListItem
+                      button
+                      component="a"
+                      href="#erfahrung-und-qualifikationen"
+                      onClick={() => setNavMenuOpen(false)}
+                    >
+                      <SliderListItemText>
+                        Erfahrung und Qualifikationen
+                      </SliderListItemText>
+                    </ListItem>
+                    <ListItem
+                      button
+                      component="a"
+                      href="#leistungen"
+                      onClick={() => setNavMenuOpen(false)}
+                    >
+                      <SliderListItemText>Leistungen</SliderListItemText>
+                    </ListItem>
+                    <ListItem
+                      button
+                      component="a"
+                      href="#kontakt-und-anfahrt"
+                      onClick={() => setNavMenuOpen(false)}
+                    >
+                      <SliderListItemText>
+                        Kontakt und Anfahrt
+                      </SliderListItemText>
+                    </ListItem>
+                  </List>
+                </SwipeableDrawer>
+              </>
+            ) : (
+              <>
+                <NavButton href="#ueber-mich">Über mich</NavButton>
+                <NavButton href="#erfahrung-und-qualifikationen">
+                  Erfahrung und Qualifikationen
+                </NavButton>
+                <NavButton href="#leistungen">Leistungen</NavButton>
+                <NavButton href="#kontakt-und-anfahrt">
+                  Kontakt und Anfahrt
+                </NavButton>
+              </>
+            )}
+            <Link href="#">
+              <NavLogo
+                alt="Logo der Physiotherapie Daniela Burkhardt"
+                src="/logo.png"
+                loading="lazy"
+              />
+            </Link>
+          </Toolbar>
+        </Container>
+      </Nav>
       <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          <NavButton href="#ueber-mich">Über mich</NavButton>
-          <NavButton href="#erfahrung-und-qualifikationen">
-            Erfahrung und Qualifikationen
-          </NavButton>
-          <NavButton href="#leistungen">Leistungen</NavButton>
-          <NavButton href="#kontakt-und-anfahrt">Kontakt und Anfahrt</NavButton>
-          <Link href="#">
-            <NavLogo
-              alt="Logo der Physiotherapie Daniela Burkhardt"
-              src="/logo.png"
-              loading="lazy"
-            />
-          </Link>
-        </Toolbar>
+        <Typography variant="h1">Daniela Burkhardt</Typography>
+        <Typography variant="h2">Über mich</Typography>
       </Container>
-    </Nav>
-    <Container maxWidth="lg">
-      <Typography variant="h1">Daniela Burkhardt</Typography>
-      <Typography variant="h2">Über mich</Typography>
-    </Container>
-  </>
-);
+    </>
+  );
+};
 
 export default Home;
