f9101e35d687e1e2a9950191eb02f2a887e6f8e9
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-09-12 20:41:57 +0200

    feat: Add basic navbar and dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { NoSsr } from "@material-ui/core";
+import { CssBaseline, NoSsr, useMediaQuery } from "@material-ui/core";
 import {
   createMuiTheme,
   MuiThemeProvider,
@@ -8,44 +8,51 @@ import "fontsource-open-sans";
 import "fontsource-permanent-marker";
 import { AppProps } from "next/app";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { useMemo } from "react";
 
 const GlobalStyle = createGlobalStyle`
-body {
-    margin: 0;
-}
+
 `;
 
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: "#A11A3D",
-    },
-    secondary: {
-      main: "#333333",
-    },
-  },
-  typography: {
-    fontFamily: "Open Sans, Roboto, Helvetica, Arial, sans-serif",
-    h1: {
-      fontFamily:
-        "Permanent Marker, Open Sans, Roboto, Helvetica, Arial, sans-serif",
-      textTransform: "uppercase",
-    },
-    h2: {
-      fontFamily:
-        "Permanent Marker, Open Sans, Roboto, Helvetica, Arial, sans-serif",
-      textTransform: "uppercase",
-      color: "#A11A3D",
-    },
-  },
-});
-
 const App = ({ Component, pageProps }): React.ReactElement<AppProps> => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+          primary: {
+            main: "#A11A3D",
+          },
+          secondary: {
+            main: "#333333",
+          },
+        },
+        typography: {
+          fontFamily: "Open Sans, Roboto, Helvetica, Arial, sans-serif",
+          h1: {
+            fontFamily:
+              "Permanent Marker, Open Sans, Roboto, Helvetica, Arial, sans-serif",
+            textTransform: "uppercase",
+          },
+          h2: {
+            fontFamily:
+              "Permanent Marker, Open Sans, Roboto, Helvetica, Arial, sans-serif",
+            textTransform: "uppercase",
+            color: "#A11A3D",
+          },
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
     <NoSsr>
       <MuiThemeProvider theme={theme}>
         <ThemeProvider theme={theme}>
           <StylesProvider injectFirst>
+            <CssBaseline />
             <GlobalStyle />
             <Component {...pageProps} />
           </StylesProvider>
